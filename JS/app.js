fbfc17ab0c43317ca8c501af434ed5ff8c58e547
chrs-m <mister.ch@gmail.com>
2022-02-22 12:56:55 +0100

    TV + rolling text

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+var options = {
+  animate: true,
+  patternWidth: 100,
+  patternHeight: 100,
+  grainOpacity: 0.2,
+  grainDensity: 1,
+  grainWidth: 3.68,
+  grainHeight: 1,
+};
+grained("#tv-grain", options);
+
+/*
+  background-color :rgb(204, 204, 204)
+  */
