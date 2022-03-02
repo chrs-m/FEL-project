efac10b91eab8563be52a46b2d4bdc37a4052fd4
chrs-m <mister.ch@gmail.com>
2022-03-02 13:16:42 +0100

    styling changes after review from teacher

--- JS/app.js
+++ JS/app.js
@@ -2,9 +2,9 @@ var options = {
   animate: true,
   patternWidth: 100,
   patternHeight: 100,
-  grainOpacity: 0.2,
+  grainOpacity: 0.13,
   grainDensity: 1,
-  grainWidth: 3.68,
+  grainWidth: 2.19,
   grainHeight: 1,
 };
 grained("#tv-grain", options);
